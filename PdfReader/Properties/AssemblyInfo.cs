9c1478c38d2f4d8e72d53c8b0ec40b30e2733523
Steve Fabry <csfabry@gmail.com>
2019-04-17 07:17:24 -0400

    Update AssemblyInfo

--- PdfReader/Properties/AssemblyInfo.cs
+++ PdfReader/Properties/AssemblyInfo.cs
@@ -1,18 +1,19 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PdfReader")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Implement IFileMgt to read PDF files")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+ [assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("CPSC")]
 [assembly: AssemblyProduct("PdfReader")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
