e9e619a5c7f74501353e59a5fb5c101b97da14f0
Steve Fabry <csfabry@gmail.com>
2019-05-01 07:24:57 -0400

    Added column scan for Excel Reader

--- XlsxReader/reader.cs
+++ XlsxReader/reader.cs
@@ -1,9 +1,12 @@
 ﻿using ExcelDataReader;
 using FileManager;
-using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Configuration;
 using System.Data;
 using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace XlsxReader
 {
@@ -17,7 +20,7 @@ namespace XlsxReader
 			using (var stream = new FileStream(filename, FileMode.Open, FileAccess.Read))
 			{
 				IExcelDataReader reader = null;
-				
+
 				switch (Path.GetExtension(filename))
 				{
 					case ".xls":
@@ -33,12 +36,66 @@ namespace XlsxReader
 					{
 						using (DataSet ds = reader.AsDataSet())
 						{
-							return new FileContents(JsonConvert.SerializeObject(ds), imageScan ? FileContents.None : FileContents.NotApplicable);
+							return new FileContents(ConvertToCSV(ds), imageScan ? FileContents.None : FileContents.NotApplicable);
 						}
 					}
 				}
 			}
 			return new FileContents("", FileContents.None);
+		}
+		private string ConvertToCSV(DataSet objDataSet)
+		{
+			StringBuilder content = new StringBuilder();
+			string colFilter = ConfigurationManager.AppSettings["ColumnFilter"];
+			List<int> cols = new List<int>();
+			if (objDataSet.Tables.Count >= 1)
+			{
+				DataTable table = objDataSet.Tables[0];
+
+				if (table.Rows.Count > 0)
+				{
+					DataRow dr1 = (DataRow)table.Rows[0];
+					int intColumnCount = dr1.Table.Columns.Count;
+					int index = 1;
+
+					//add column names
+					foreach (DataColumn item in dr1.Table.Columns)
+					{
+						content.Append(string.Format("\"{0}\"", item.ColumnName));
+						if (index < intColumnCount)
+							content.Append(",");
+						else
+							content.Append("\r\n");
+						index++;
+						if ( colFilter != null && colFilter.Length > 0)
+						{
+							if (Regex.IsMatch(item.ColumnName, colFilter))
+								cols.Add(item.Ordinal);
+						}
+					}
+
+					//add column data
+					foreach (DataRow currentRow in table.Rows)
+					{
+						string strRow = string.Empty;
+						for (int y = 0; y <= intColumnCount - 1; y++)
+						{
+							bool addCol = true;
+							if (cols.Count > 0 && cols.Contains(y) == false)
+								addCol = false;
+
+							if (addCol)
+								strRow += "\"" + currentRow[y].ToString() + "\"";
+
+							if (y < intColumnCount - 1 && y >= 0)
+								strRow += ",";
+						}
+						content.Append(strRow + "\r\n");
+					}
+				}
+			}
+
+			return content.ToString();
 		}
 	}
 }
